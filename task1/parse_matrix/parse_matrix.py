8df02f042e7079cf3cd1888b647e42781ba9996f
lipkij <lipkij@gmail.com>
2022-12-03 18:32:34 +0400

    desicion1

---
+++ task1/parse_matrix/parse_matrix.py
@@ -0,0 +1,20 @@
+
+
+def parse_matrix(raw: str) -> list[int]:
+    out = []
+    lasdigit = ''
+    last = False
+    for i in raw:
+        if i.isnumeric() :
+            if last:
+                lasdigit = lasdigit + i
+                last = True
+            else:
+                lasdigit = i
+                last = True
+        else:
+            if last :
+                out.append(int(lasdigit))
+                last = False
+                lasdigit = ''
+    return out
---
+++ task1/test_parse_matrix.py
@@ -0,0 +1,29 @@
+import unittest
+from parse_matrix.parse_matrix import parse_matrix
+
+INPUT = '''
+        +-----+-----+-----+-----+
+        |  10 |  20 |  30 |  40 |
+        +-----+-----+-----+-----+
+        |  50 |  60 |  70 |  80 |
+        +-----+-----+-----+-----+
+        |  90 | 100 | 110 | 120 |
+        +-----+-----+-----+-----+
+        | 130 | 140 | 150 | 160 |
+        +-----+-----+-----+-----+
+        '''
+
+MATRIX = [
+    10, 20, 30, 40,
+    50, 60, 70, 80,
+    90, 100, 110, 120,
+    130, 140, 150, 160,
+]
+
+class TestMatrixParsing(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual(parse_matrix(INPUT), MATRIX)
+
+if __name__ == '__main__':
+    unittest.main()
